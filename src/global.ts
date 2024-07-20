9d9c0dd1a09c8eb233a7c62e5579e67c0ed9c17c
The Old Jack <daymonblackgamer@gmail.com>
2024-07-20 16:57:40 -0300

    Implementação do styled component

--- src/global.ts
+++ src/global.ts
@@ -1,9 +1,13 @@
-:root {
+import { createGlobalStyle } from 'styled-components'
+import styled from 'styled-components'
+
+const EstiloGlobal = createGlobalStyle`
+  :root {
   --cor-principal: #a7727d;
   --cor-secundaria: #f9f5e7;
 }
 
-* {
+  * {
   margin: 0;
   padding: 0;
   box-sizing: border-box;
@@ -14,15 +18,16 @@
 body {
   padding-bottom: 120px;
 }
+`
 
-.container {
+export const Container = styled.div`
   max-width: 1024px;
   width: 100%;
   margin: 0 auto;
-}
 
-@media (max-width: 1024px) {
-  .container {
+  @media (max-width: 1024px) {
     max-width: 80%;
   }
-}
+`
+
+export default EstiloGlobal
